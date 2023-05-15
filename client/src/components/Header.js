135147daf27b8ebd6fa5d2886bfd0faca9cbf171
LimJaeSub <liso_o@naver.com>
2023-05-15 13:38:02 +0900

    Header
    Headerfin

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 import Logoimg from "../props/image/Logo.png";
 import Hambugericon from "../props/icon/hambuger.svg";
 
+import DropDown from "./DropDown";
+
 const HeaderWrap = styled.div`
   display: flex;
+  position: relative;
   align-items: center;
   justify-content: space-around;
   height: 80px;
@@ -22,27 +26,39 @@ const Logo = styled.img`
   width: 60px;
   height: 30px;
   margin: 15px;
+  cursor: pointer;
 `;
 const HeaderRight = styled.div`
   width: 20%;
   display: flex;
   justify-content: right;
   align-items: center;
+  position: relative;
 `;
 const Hambuger = styled.img`
   width: 30px;
   height: 20px;
   margin: 10px;
+  cursor: pointer;
 `;
+
 function Header() {
+  const [isShow, setIsShow] = useState(false);
+
+  const toggleShowDropDown = () => {
+    setIsShow(!isShow);
+  };
   return (
     <HeaderWrap>
       <HeaderLeft>
-        <Logo src={Logoimg} />
+        <Link to="/">
+          <Logo src={Logoimg} />
+        </Link>
         COZ Shopping
       </HeaderLeft>
       <HeaderRight>
-        <Hambuger src={Hambugericon} />
+        <Hambuger src={Hambugericon} onClick={toggleShowDropDown} />
+        {isShow && <DropDown />}
       </HeaderRight>
     </HeaderWrap>
   );
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Header from "../components/Header.js";
+import Footer from "../components/Footer.js";
+import styled from "styled-components";
+
+const Mainsection = styled.div`
+  width: 100%;
+  height: 80vh;
+  background-color: blue;
+`;
 function MainPage() {
   return (
     <>
       <Header />
+      <Mainsection>여기메인임</Mainsection>
+      <Footer />
     </>
   );
 }
