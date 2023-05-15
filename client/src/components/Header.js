e25b39103f2694cd72db2a2f4970b8142a3d449b
LimJaeSub <liso_o@naver.com>
2023-05-15 09:50:23 +0900

    Header #1
    Header의 전체적인 모습

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,51 @@
 import React from "react";
+import styled from "styled-components";
 
+import Logoimg from "../props/image/Logo.png";
+import Hambugericon from "../props/icon/hambuger.svg";
+
+const HeaderWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 80px;
+  box-shadow: 0 8px 8px 0px rgb(0, 0, 0, 0.1);
+`;
+const HeaderLeft = styled.div`
+  width: 40%;
+  font-weight: 700;
+  font-size: 32px;
+  display: flex;
+  align-items: center;
+`;
+const Logo = styled.img`
+  width: 60px;
+  height: 30px;
+  margin: 15px;
+`;
+const HeaderRight = styled.div`
+  width: 20%;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+`;
+const Hambuger = styled.img`
+  width: 30px;
+  height: 20px;
+  margin: 10px;
+`;
 function Header() {
-  return <div>Header</div>;
+  return (
+    <HeaderWrap>
+      <HeaderLeft>
+        <Logo src={Logoimg} />
+        COZ Shopping
+      </HeaderLeft>
+      <HeaderRight>
+        <Hambuger src={Hambugericon} />
+      </HeaderRight>
+    </HeaderWrap>
+  );
 }
 
 export default Header;
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Header from "../components/Header.js";
 function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default MainPage;
